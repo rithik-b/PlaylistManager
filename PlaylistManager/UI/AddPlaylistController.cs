ea3416c863c92e07a56d8eeee507499cfa7d444f
rithik <rithik@DESKTOP-02OF0BP>
2020-12-27 18:12:41 -0500

    Add project files.

---
+++ PlaylistManager/UI/AddPlaylistController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using static BeatSaberMarkupLanguage.Components.CustomListTableData;
+using System.Reflection;
+using UnityEngine;
+using PlaylistLoaderLite;
+using HMUI;
+
+namespace PlaylistManager.UI
+{
+    class AddPlaylistController : NotifiableSingleton<AddPlaylistController>
+    {
+        private StandardLevelDetailViewController standardLevel;
+
+        [UIComponent("list")]
+        public CustomListTableData customListTableData;
+
+        //Currently selected song data
+        public IPreviewBeatmapLevel level;
+
+        [UIComponent("add-button")]
+        private Transform addButtonTransform;
+
+        [UIComponent("modal")]
+        private ModalView modal;
+
+        private Playlist[] loadedplaylists;
+        internal void Setup()
+        {
+            standardLevel = Resources.FindObjectsOfTypeAll<StandardLevelDetailViewController>().First();
+            BSMLParser.instance.Parse(BeatSaberMarkupLanguage.Utilities.GetResourceContent(Assembly.GetExecutingAssembly(), "PlaylistManager.UI.AddPlaylist.bsml"), standardLevel.transform.Find("LevelDetail").gameObject, this);
+            addButtonTransform.localScale *= 0.7f;
+        }
+
+        [UIAction("button-click")]
+        internal void ShowPlaylists()
+        {
+            level = standardLevel.selectedDifficultyBeatmap.level;
+            customListTableData.data.Clear();
+            loadedplaylists = LoadPlaylistScript.loadedPlaylists;
+
+            foreach (Playlist playlist in loadedplaylists)
+            {
+                String subName = String.Format("{0} songs", playlist.beatmapLevelCollection.beatmapLevels.Length);
+                customListTableData.data.Add(new CustomCellInfo(playlist.collectionName, subName, playlist.coverImage));
+            }
+
+            customListTableData.tableView.ReloadData();
+            customListTableData.tableView.ScrollToCellWithIdx(0, TableViewScroller.ScrollPositionType.Beginning, false);
+        }
+
+        [UIAction("select-cell")]
+        internal void OnCellSelect(TableView tableView, int index)
+        {
+            loadedplaylists[index].editBeatMapLevels(loadedplaylists[index].beatmapLevelCollection.beatmapLevels.Append<IPreviewBeatmapLevel>(level).ToArray());
+            customListTableData.tableView.ClearSelection();
+            modal.Hide(true);
+        }
+    }
+}
