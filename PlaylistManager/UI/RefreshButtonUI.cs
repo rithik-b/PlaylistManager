745493d31261b66157ebe76ca88a9d55a422523b
rithik bhatia <rithikbhatia12@gmail.com>
2021-02-07 19:54:20 -0500

    NOT OVER YET Removed RequestRefresh from RefreshButton as it only breaks stuff rn, will ask about how to request refresh properly later

--- PlaylistManager/UI/RefreshButtonUI.cs
+++ PlaylistManager/UI/RefreshButtonUI.cs
@@ -3,7 +3,6 @@ using BeatSaberMarkupLanguage.MenuButtons;
 using PlaylistManager.Utilities;
 using SongCore;
 using System;
-using System.Threading.Tasks;
 using Zenject;
 
 namespace PlaylistManager.UI
@@ -13,13 +12,11 @@ namespace PlaylistManager.UI
         public MenuButton _refreshButton;
         internal ProgressBar _progressBar;
         const int MESSAGE_TIME = 5;
-        const string REQUEST_SOURCE = "PlaylistManager (Plugin)";
 
         public void Initialize()
         {
             _refreshButton = new MenuButton("Refresh Playlists", "Refresh Songs & Playlists", RefreshButtonPressed, true);
             MenuButtons.instance.RegisterButton(_refreshButton);
-            Loader.SongsLoadedEvent += SongsLoaded;
         }
 
         private void SongsLoaded(Loader _, System.Collections.Concurrent.ConcurrentDictionary<string, CustomPreviewBeatmapLevel> songs)
@@ -28,12 +25,11 @@ namespace PlaylistManager.UI
             {
                 _progressBar = ProgressBar.Create();
             }
-
-            PlaylistLibUtils.playlistManager.RequestRefresh(REQUEST_SOURCE); // For now does nothing, need to ask the author of manager to implement actual refresh
             int numPlaylists = PlaylistLibUtils.playlistManager.GetAllPlaylists(true).Length;
 
             _progressBar.enabled = true;
             _progressBar.ShowMessage(string.Format("\n{0} playlists loaded.", numPlaylists), MESSAGE_TIME);
+            Loader.SongsLoadedEvent -= SongsLoaded;
         }
 
         public void Dispose()
@@ -48,6 +44,7 @@ namespace PlaylistManager.UI
         {
             if (!Loader.AreSongsLoading)
                 Loader.Instance.RefreshSongs(fullRefresh: false);
+            Loader.SongsLoadedEvent += SongsLoaded;
         }
     }
 }
