a5f620e2268bb2e9a67658eeea54f0eec1723f7b
Meivyn <793322+Meivyn@users.noreply.github.com>
2024-06-18 22:20:20 -0400

    Show level collection header when playlist is empty

--- PlaylistManager/HarmonyPatches/LevelCollectionViewController_SetData.cs
+++ PlaylistManager/HarmonyPatches/LevelCollectionViewController_SetData.cs
@@ -9,9 +9,17 @@ namespace PlaylistManager.HarmonyPatches
     {
         internal static IReadOnlyCollection<BeatmapLevel> beatmapLevels { get; private set; }
 
-        internal static void Prefix(BeatmapLevel[] beatmapLevels)
+        private static void Prefix(BeatmapLevel[] beatmapLevels)
         {
             LevelCollectionViewController_SetData.beatmapLevels = beatmapLevels ?? Array.Empty<BeatmapLevel>();
+        }
+
+        private static void Postfix(LevelCollectionViewController __instance)
+        {
+            if (beatmapLevels.Count == 0)
+            {
+                __instance._levelCollectionTableView.gameObject.SetActive(true);
+            }
         }
     }
 }
